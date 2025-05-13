fca812d45f84cf21dc1bfb4728653bf7e6242210
Iiemb <iiem.ahmad@ukur.com>
2025-05-13 19:25:33 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "564607343"
+    serviceWorkerVersion: "2280674335"
   }
 });
